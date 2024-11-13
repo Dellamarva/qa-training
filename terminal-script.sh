21b93f5df6394bc20e5fa21676d55a3202dd656d
Greg Dellamura <g.dellamura@Gregs-MacBook-Pro-2.local>
2024-11-13 11:44:22 -0500

    Added scripting practice

---
+++ terminal-script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Shell script
+#Write terminal commands here
+
+#chmod +x terminal-script.sh changes permission of the script
+
+echo Start shell script
+ls
+pwd
+echo End shell script
