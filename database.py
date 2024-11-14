c3203ae00a1d59a5c5fe83144434f45176394b27
Greg Dellamura <g.dellamura@Gregs-MacBook-Pro-2.local>
2024-11-14 11:51:37 -0500

    ETL Practice

--- database.py
+++ database.py
@@ -1,45 +1,76 @@
 import sqlite3
 
 
-def create_user():
-    conn = sqlite3.connect('mydatabase.db')
+def create_user(name, age, database_name):
+    conn = sqlite3.connect('{}.db'.format(database_name))
     c = conn.cursor()
-    c.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Alice', 30))
-
+    c.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))
     conn.commit()
     conn.close()
 
-def print_database():
-            conn = sqlite3.connect('mydatabase.db')
-            c = conn.cursor()
+def update_user(name, age, database_name):
+    conn = sqlite3.connect('{}.db'.format(database_name))
+    c = conn.cursor()
+    c.execute("UPDATE users SET age = ? WHERE name = ?", (age, name))
+    conn.commit()
+    conn.close()
 
-            c.execute("SELECT * FROM users WHERE id = 2")
-            rows = c.fetchall()
+def delete_user(name, database_name):
+    conn = sqlite3.connect('{}.db'.format(database_name))
+    c = conn.cursor()
+    c.execute("DELETE FROM users WHERE name = ?", (name,))
+    conn.commit()
+    conn.close()
 
-            for row in rows:
-                        print(row)
+def get_user(name, database_name):
+    conn = sqlite3.connect('{}.db'.format(database_name))
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE name = ?", (name,))
+    user = c.fetchone()
+    conn.close()
+    return user
 
-            conn.close()
+def get_user_table(database_name):
+    conn = sqlite3.connect('{}.db'.format(database_name))
+    c = conn.cursor()
+    c.execute("SELECT * FROM users", ())
+    all_users = c.fetchall()
+    conn.close()
+    return all_users
 
+def delete_users_from_user_table(database_name):
+    conn = sqlite3.connect('{}.db'.format(database_name))
+    c = conn.cursor()
+    c.execute("DELETE FROM users")
+    conn.commit()
+    conn.close()
 
-def create_database():
-            # Create a connection to the database (it will be created if it doesn't exist)
-            conn = sqlite3.connect('mydatabase.db')
+def print_database(database_name):
+    conn = sqlite3.connect('{}.db'.format(database_name))
+    c = conn.cursor()
+    c.execute("SELECT * FROM users")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
 
-            # Create a cursor object to execute SQL commands
-            cursor = conn.cursor()
+def create_database(database_name):
+    # Create a connection to the database (it will be created if it doesn't exist)
+    conn = sqlite3.connect('{}.db'.format(database_name))
 
-            # Create a table (if it doesn't exist)
-            cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+    # Create a cursor object to execute SQL commands
+    cursor = conn.cursor()
+
+    # Create a table (if it doesn't exist)
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     name TEXT,
     age INTEGER
                 )''')
 
-            # Commit the changes
-            conn.commit()
+    # Commit the changes
+    conn.commit()
 
-            # Close the connection
-            conn.close()
+    # Close the connection
+    conn.close()
 
-            print("SQLite database created successfully!")
+    print("SQLite database named {} created successfully".format(database_name))
